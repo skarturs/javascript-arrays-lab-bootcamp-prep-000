e87ab0d350f201bc059c8ac82a6dbf672a26b1ba
Arturs Teihmanis <arturs.teihmanis@gmail.com>
2017-11-21 20:49:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = "I don't do much."
-var kittens = ['Milo', 'Otis', 'Garfield'];
-var kakis1 = 'Ralph';
-var kakis2 = 'Bob';
+var kittens = ["Milo", "Otis", "Garfield"];
+var kakis1 = "Ralph";
+var kakis2 = "Bob";
 
 function destructivelyAppendKitten(kittens, kakis1) {
   kittens.push(kakis1);
